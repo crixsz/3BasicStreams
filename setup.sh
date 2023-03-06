a34a60921bd158385a1ecc5d230431ab4faec89a
crixsz <89030463+crixsz@users.noreply.github.com>
2023-03-06 15:06:12 +0800

    added uninstaller

--- setup.sh
+++ setup.sh
@@ -19,6 +19,13 @@ function uninstaller() {
     rm -rf /etc/systemd/system/qbittorrent-nox.service
     rm -rf /etc/systemd/system/filebrowser.service
     apt-get remove emby-server
+    # The text that you want to replace
+    old_text="netstat"
+
+    # The text that you want to replace it with
+    new_text=""
+    # Use sed to replace the text and save the changes to the same file
+    sed -i "s/$old_text/$new_text/g" .profile
     echo "[Uninstall Successfully]"
 
 }
